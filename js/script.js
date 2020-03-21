65a3dae511706e81f7402de8f6a94d72512e730e
Andrey <vokuev-andrei@mail.ru>
2020-03-21 16:06:43 +0300

    Это нащ первый коммит

---
+++ js/script.js
@@ -0,0 +1,138 @@
+
+let second = 2;
+const pi = 3.14;
+console.log(second);
+
+/*let person = {
+    name: 'John',
+    age: 25,
+    isMarried: false
+};
+
+console.log(person.name);*/
+
+
+let arr = ['plum.png','orange.jpg', 'apple.bmp',];
+console.log(arr[0])
+
+
+
+//alert("Hello world");
+
+/*let answer = confirm("Are yiu here?");
+ 
+console.log(answer);
+*/
+
+let incr = 10,
+    decr = 10;
+
+
+
+console.log(++incr);
+console.log(--decr);
+
+
+console.log(5%2);
+
+console.log('2' === 2);
+
+
+
+let isChecked = true;
+    isClose = false;
+
+console.log(isChecked || !isClose);
+
+
+
+
+
+
+
+
+
+/*0Klass1 = function () {}
+Klass1.prototype.foo = function () {
+    log('foo');
+}
+Klass1.prototype.bar = function () {
+    log('bar');
+}
+
+
+
+Klass2 = function () {
+    var foo = function () {
+        log('foo');
+    },
+    bar = function () {
+        log('bar');
+    };
+
+    return {
+        foo: foo,
+        bar: bar
+    }
+}
+
+
+var FooFunction = function () {
+    log('foo');
+};
+var BarFunction = function () {
+    log('bar');
+};
+
+Klass3 = function () {
+    return {
+        foo: FooFunction,
+        bar: BarFunction
+    }
+}
+
+
+var i = 1000,
+    objs = [];
+while (i--) {
+    var o = new Klass1()
+    objs.push(new Klass1());
+    o.bar;
+    o.foo;
+}
+
+
+var i = 1000,
+    objs = [];
+while (i--) {
+    var o = Klass2()
+    objs.push(Klass2());
+    o.bar;
+    o.foo;
+}
+
+
+var i = 1000,
+    objs = [];
+while (i--) {
+    var o = Klass3()
+    objs.push(Klass3());
+    o.bar;
+    o.foo;
+}
+
+
+
+
+
+
+
+
+
+'use strict';
+
+let second = 2;
+const pi =3.14;
+console.log(second);
+
+console.log(pi);*/
